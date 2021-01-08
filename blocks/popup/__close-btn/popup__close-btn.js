2413226ada1a9d44e7418aef58097183eb254f78
Alucardik <ichekk@yandex.ru>
2021-01-08 15:47:12 +0300

    feat: added edit overview

---
+++ blocks/popup/__close-btn/popup__close-btn.js
@@ -0,0 +1,5 @@
+let closeBtn = document.querySelector(".popup__close-btn");
+
+closeBtn.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
+})
---
+++ blocks/popup/popup.js
@@ -0,0 +1,6 @@
+let editBtn = document.querySelector(".profile__btn_type_edit");
+let popup = document.querySelector(".popup");
+
+editBtn.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
+})
